5a932f34f9030909fca9a1156ae7b32b6a6a1965
Ali Aghajani <105543467+peymangiit@users.noreply.github.com>
2023-02-25 00:01:01 +0330

    show products on main page

--- index.php
+++ index.php
@@ -1,7 +1,51 @@
 <?php
   include("includes/header.php")
 ?>
-محصولات               
+محصولات  
+<?php
+
+  $link = mysqli_connect("localhost","root","","shop_db");
+  if (mysqli_connect_errno())
+    exit("خطایی به شرح زیر رخ داده".mysqli_connect_error());
+
+    $query = "SELECT * FROM products";
+    $query_result = mysqli_query($link,$query);
+?>
+<table style="width: 100%; border:1px;">
+  <tr>
+
+    <?php
+      $counter =0;
+      while ($row=mysqli_fetch_array($query_result)){
+        $counter++;
+    ?>
+
+    <td style="border-style:dotted; vertical-align:top; width:33%">
+
+      <h4><?php echo ($row['pro_name'])?></h4>
+      <a href="product_detail.php?id=<?php echo($row['pro_code'])?> style=text-decoration:none;">
+        <img src="<?php echo ($row['pro_image'])?>" width="150px" height="250px" />
+      </a>
+      <br>
+      <p> قیمت : <?php echo($row['pro_price']) ?> &nbsp ريال </p>
+      <br>
+      <p> تعداد موجودی : <span style="color:red;"><?php echo($row['pro_qty']) ?></span></p>
+      <br>
+
+    </td> 
+    
+  <?php 
+    if ($counter%3 == 0)
+        echo("<tr></tr>");
+  }//end of while
+    if ($counter%3 != 0)
+      echo("</tr>");
+    ?>
+
+  </tr>
+</table>
+
+
 <?php 
   include("includes/footer.php");
 ?>
