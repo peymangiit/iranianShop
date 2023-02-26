d98d2729464b85951163299ed5c6b08d2e486734
Ali Aghajani <105543467+peymangiit@users.noreply.github.com>
2023-02-26 14:07:12 +0330

    add product_detail page

--- index.php
+++ index.php
@@ -24,14 +24,15 @@
 
       <h4><?php echo ($row['pro_name'])?></h4>
       <a href="product_detail.php?id=<?php echo($row['pro_code'])?> style=text-decoration:none;">
-        <img src="<?php echo ($row['pro_image'])?>" width="150px" height="250px" />
+        <img src="<?php echo ($row['pro_image'])?>" width="200px" height="250px" />
       </a>
       <br>
-      <p> قیمت : <?php echo($row['pro_price']) ?> &nbsp ريال </p>
-      <br>
+      <p > قیمت : <?php echo($row['pro_price']) ?> &nbsp ريال </p>
+      <p> توضیحات : <?php echo(substr($row['pro_detail'],0,12)) ?>... </p>
       <p> تعداد موجودی : <span style="color:red;"><?php echo($row['pro_qty']) ?></span></p>
-      <br>
-
+      
+      <a id="<?php echo($row['pro_code']); ?>" href="product_detail.php" style="font-weight: bold;color:green; text-decoration:none"> توضیحات تکمیلی و خرید </a>
+        
     </td> 
     
   <?php 
---
+++ product_detail.php
@@ -0,0 +1,46 @@
+<?php
+include("includes/header.php")
+?>
+
+<?php
+$link = mysqli_connect("localhost","root","","shop_db");
+if (mysqli_connect_errno())
+    exit("خطایی به شرح زیر رخ داده".mysqli_connect_error());
+
+if (isset($_GET['id']))
+    $pro_code = $_GET['id'];
+
+$query = "SELECT * FROM products WHERE pro_code = '$pro_code'";
+$q_result = mysqli_query($link,$query);
+
+?>
+
+<table style="width: 100%;">
+<tr>
+
+<?php
+    if ($row=mysqli_fetch_array($q_result)){
+?>
+<td style="border-style:dotted; vertical-align:top;">
+
+<h4><?php echo ($row['pro_name'])?></h4>
+
+<br>
+<p > قیمت : <?php echo($row['pro_price']) ?> &nbsp ريال </p>
+<p> توضیحات : <?php echo($row['pro_detail']) ?> </p>
+<p> تعداد موجودی : <span style="color:red;"><?php echo($row['pro_qty']) ?></span></p>
+
+<a id="<?php echo($row['pro_code']); ?>" href="order.php" style="font-weight: bold;color:green; text-decoration:none"> ثبت و خرید </a>
+
+  
+</td>
+
+<td><img src="<?php echo ($row['pro_image'])?>"  /></td>
+    
+    </tr>
+</table>
+
+<?php
+    }// if end
+include("includes/footer.php")
+?>
